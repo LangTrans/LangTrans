a6e89407a351100bdba332c3f2c65b308c61cf41
Bijin Regi Panicker <bijinregipanicker@gmail.com>
2021-01-24 10:43:06 +0530

    Rearrange

--- LangTrans.py
+++ LangTrans.py
@@ -77,3 +77,18 @@ def main(content,exlocation,plocation):
 		if not loop or count==lmit:
 			break
 	return content
+if __name__ == "__main__":
+	from sys import argv,exit
+	if len(argv)==1 or (len(argv)==2 and argv[1]=="-h"):
+		print("Arg usage: <SoureFileName> <OutputFileName> <SyntaxRepr> <PatternRepr>")
+		print("SyntaxRepr,PatternRepr: without extension(.yaml) ")
+		exit()
+	elif len(argv)<5:
+		print("Error: Insufficient number of arguments")
+		exit()
+	try:
+		targetcode = main(open(argv[1]).read(), argv[3]+".yaml", argv[4]+".yaml")
+		open(argv[2],"w").write(targetcode)
+		print(targetcode)
+	except Exception as err:
+		print("Error:",err)
