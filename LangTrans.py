e880b1f3ef43f210cf1386221b61000a78dc19f4
Bijin Regi Panicker <bijinregipanicker@gmail.com>
2021-02-21 17:56:28 +0530

    Readable Variable Names

--- LangTrans.py
+++ LangTrans.py
@@ -14,129 +14,133 @@ def extract(spattern):
 
     :param spattern: Dictionary with yaml file details
     :type spattern: dic
-    :return: option(repalce,eachline),regex,token_names
+    :return: option(replace,eachline),regex,token_names
     :rtype: dic,dic,list
     """
-    fdict = dict()
-    pt = dict()
-    cat = []
-    for name, value in spattern.items():
-        if name[0] == "_":
-            value["tokens"] = spattern[name[2:]]["tokens"]
-        pt.update({name: comp(value["regex"], MULTILINE)})
-        eachline = dict()
-        replas = dict()
-        for w in value["tokens"]:
-            if w in value:
-                if "eachline" in value[w]:
-                    eachline.update({w: value[w]["eachline"]})
-                if "replace" in value[w]:
-                    replas.update(
+    options = dict()
+    regexs = dict()
+    tknames = []
+    for part, sdef in spattern.items():
+        if part[0] == "_":
+            sdef["tokens"] = spattern[part[2:]]["tokens"]
+        regexs.update({part: comp(sdef["regex"], MULTILINE)})
+        oneachline = dict()
+        replase = dict()
+        for tkname in sdef["tokens"]:
+            if tkname in sdef:
+                if "eachline" in sdef[tkname]:
+                    oneachline.update({tkname: sdef[tkname]["eachline"]})
+                if "replace" in sdef[tkname]:
+                    replase.update(
                         {
-                            w: [
-                                (comp(q[0]), q[1]) if 1 < len(q) else (comp(q[0]), "")
-                                for q in value[w]["replace"]
+                            tkname: [
+                                (comp(repregex[0]), repregex[1])
+                                if 1 < len(repregex)
+                                else (comp(repregex[0]), "")
+                                for repregex in sdef[tkname]["replace"]
                             ]
                         }
                     )
-        fdict.update({name: [eachline, replas]})
-        cat.append(value["tokens"])
-    return fdict, pt, cat
+        options.update({part: [oneachline, replase]})
+        tknames.append(sdef["tokens"])
+    return options, regexs, tknames
 
 
-def matching(patterns, cats, content):
+def matching(patterns, tknames, content):
     """
     This function matches tokens from code with regular expressions
 
     :param patterns: Dictionary regular expression with its token name
     :type patterns: dict
-    :param cats: Token names
-    :type cats: list
+    :param tknames: Token names
+    :type tknames: list
     :param content: Tokens are matched from this
     :type content: str
     :return: Matched tokens only and Full match of regular expression
     :rtype: dic,dic
     """
-    matches = dict()
-    fullmatch = dict()
-    for (name, pattern), cat in zip(patterns.items(), cats):
-        fullmatch[name] = [i.group() for i in pattern.finditer(content)]
-        if len(cat) == 1:
-            matches[name] = [{c: mat} for mat, c in zip(pattern.findall(content), cat)]
+    tknmatches = dict()
+    partmatches = dict()
+    for (part, pattern), tknames in zip(patterns.items(), tknames):
+        partmatches[part] = [i.group() for i in pattern.finditer(content)]
+        if len(tknames) == 1:
+            tknmatches[part] = [
+                {tkname: matches}
+                for matches, tkname in zip(pattern.findall(content), tknames)
+            ]
             continue
-        matches[name] = [
-            {c: i for i, c in zip(mat, cat)} for mat in pattern.findall(content)
+        tknmatches[part] = [
+            {tkname: match for match, tkname in zip(matches, tknames)}
+            for matches in pattern.findall(content)
         ]
-    return matches, fullmatch
+    return tknmatches, partmatches
 
 
-def main(content, exlocation, plocation):
+def main(content, slocation, tlocation):
     """
     This is main function convert new syntax to orginal syntax
 
     :param content: Code with new syntax
-    :param exlocation: Yaml file location containing regular expression for new syntax
-    :param plocation: Yaml file location containing pattern of orginal syntax
+    :param slocation: Yaml file location containing regular expression for new syntax
+    :param tlocation: Yaml file location containing pattern of original syntax
     :type content: str
-    :type exlocation: str
-    :type plocation: str
-    :return: Return code with orginal syntax
+    :type slocation: str
+    :type tlocation: str
+    :return: Return code with original syntax
     :rtype: str
     """
-    spattern = load(open(exlocation).read(), Loader=SafeLoader)  # Source
-    tpattern = load(open(plocation).read(), Loader=SafeLoader)  # Target
+    spattern = load(open(slocation).read(), Loader=SafeLoader)  # Source
+    tpattern = load(open(tlocation).read(), Loader=SafeLoader)  # Target
     loop = False
-    lmit = 7
+    loplimit = 7
     if "settings" in spattern:
         settings = spattern["settings"]
         if "loop" in settings:
             loop = settings["loop"]
         if "looplimit" in settings:
-            lmit = settings["looplimit"]
+            loplimit = settings["looplimit"]
         if "variables" in settings:
-            var = settings["variables"]
-            for k, v in spattern.items():
-                if k != "settings":
-                    r = v["regex"]
-                    for n, rg in var.items():
-                        if "<" + n + ">" in r:
-                            r = r.replace("<" + n + ">", rg)
-                    spattern[k]["regex"] = r
+            for part, sdef in spattern.items():
+                if part != "settings":
+                    rv = sdef["regex"]  # regex with var
+                    for varname, rgx in settings["variables"].items():
+                        if "<" + varname + ">" in rv:  # regex without var
+                            rv = rv.replace("<" + varname + ">", rgx)
+                    spattern[part]["regex"] = rv
         del spattern["settings"]
-    fdict, pt, cats = extract(spattern)
+    options, regexs, tokens = extract(spattern)
     del spattern
-    count = 0
+    lopcount = 0
     while 1:
-        matches, fullmatch = matching(pt, cats, content)
-        if not any(len(i) != 0 for i in fullmatch.values()):
+        tknmatches, partmatches = matching(regexs, tokens, content)
+        if not any(len(i) != 0 for i in partmatches.values()):
             break
-        for i in matches:
-            forline, replacer = fdict[i]  # retrieving settings for forline and replace
-            if i[0] != "_":  # Find two regex extract with one pattern
-                rp = tpattern[i]
+        for part in tknmatches:  # retrieving settings for oneachline and replace
+            oneachline, replacer = options[part]
+            if part[0] != "_":  # Find two regex extract with one pattern
+                pattern = tpattern[part]
             else:
-                rp = tpattern[i[2:]]
-            for j, f in zip(matches[i], fullmatch[i]):
-                rpn = rp  # temporary pattern
-                for k, v in j.items():
-                    if k in forline:  # For forline option
-                        line = forline[k]
-                        v = "\n".join(
+                pattern = tpattern[part[2:]]
+            for tknmatch, partmatch in zip(tknmatches[part], partmatches[part]):
+                for tkname, match in tknmatch.items():
+                    if tkname in oneachline:  # For oneachline option
+                        line = oneachline[tkname]
+                        match = "\n".join(
                             [
                                 line.replace("<line>", l)
-                                for l in v.split("\n")
+                                for l in match.split("\n")
                                 if l.strip() != ""
                             ]
                         )
-                    if k in replacer:  # For replace option
-                        for rval in replacer[k]:
-                            v = sub(*rval, v)
-                    rpn = rpn.replace(
-                        f"<{k}>", v
-                    )  # Replacing pattern tokens expression with tokens
-                content = content.replace(f, rpn)  # Replacing whole block
-        count += 1
-        if not loop or count == lmit:
+                    if tkname in replacer:  # For replace option
+                        for rgx in replacer[tkname]:
+                            match = sub(*rgx, match)
+                    # Replacing pattern tokens expression with tokens
+                    temp_pattern = pattern.replace(f"<{tkname}>", match)
+                # Replacing whole block
+                content = content.replace(partmatch, temp_pattern)
+        lopcount += 1
+        if not loop or lopcount == loplimit:
             break
     return content
 
